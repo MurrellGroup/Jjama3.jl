facf0b906d903bda008f7a7e713be8f690be8e81
Documenter.jl <documenter@juliadocs.github.io>
2024-11-12 23:30:26 +0000

    build based on 7439500

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Jjama3","category":"page"},{"location":"#Jjama3","page":"Home","title":"Jjama3","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Jjama3.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Jjama3]","category":"page"}]
+}
